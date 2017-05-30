08456a0b1121569ce14d1945f25176d4ddf3c865
Valdeci Gomes <coderade@outlook.com>
2017-05-29 23:48:35 -0300

    Generate main controller to tests purporses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'main/index'
+
+  resources :achievement, only: [:new, :create]
+
+  root to: 'main#index'
 end
