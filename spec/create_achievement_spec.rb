9f43ca73ef579ad3a98a79fbec7ce85b3058b9bb
Valdeci Gomes <coderade@outlook.com>
2017-05-29 23:00:55 -0300

    Add Create achievement spec.

---
+++ spec/create_achievement_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'create new achievement' do
+  scenario 'create new achievement with valid data' do
+    visit('/')
+    click_on('New Achievement')
+
+    fill_in('Title', with: 'Read a book')
+    fill_in('Description', with: 'Excellent read')
+    select('Public', from: 'Privacy')
+    check('Feature achievement')
+    attach_file('Cover', "#{Rails.root}/spec/fixtures/cover_image.png")
+    click_on('Create Achievement')
+
+    expect(page).to have_content('Achievement has been created')
+    expect(Achievement.last.title).to eq('Read a book')
+  end
+end
