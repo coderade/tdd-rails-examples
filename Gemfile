c2415e52a3c9377e6deac8d371a28093f26fcbf4
Valdeci Gomes <coderade@outlook.com>
2017-06-06 23:46:16 -0300

    Add and set the Devise gem to the project.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,10 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Add the devise
+gem 'devise'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'factory_girl_rails'
+require 'devise'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -55,4 +57,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers
 end
