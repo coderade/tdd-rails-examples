2443ac213477dfed6149615b344dac9ff7160d9b
Valdeci Gomes <coderade@outlook.com>
2017-06-08 00:09:48 -0300

    Fixes Create Achievement test login issues.
    Fixes Create Achievement test login issues using the Devise login form.

--- spec/create_achievement_spec.rb
+++ spec/create_achievement_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
+require_relative 'support/login_form'
 require_relative 'support/new_achievement_form'
 
 feature 'create new achievement' do
-  let(:new_achievement_form) { NewAchievementForm.new }
+  let(:new_achievement_form) {NewAchievementForm.new}
+  let(:login_form) {LoginForm.new}
+  let(:user) {FactoryGirl.create :user}
+
+  before do
+    login_form.visit_page.login_as(user)
+  end
 
   scenario 'create new achievement with valid data' do
     new_achievement_form.visit_page.fill_in_with(
---
+++ spec/support/login_form.rb
@@ -0,0 +1,15 @@
+class LoginForm
+  include Capybara::DSL
+
+  def visit_page
+    visit('/users/sign_in')
+    self
+  end
+
+  def login_as(user)
+    fill_in('Email', with: user.email)
+    fill_in('Password', with: user.password)
+    click_on('Log in')
+  end
+
+end
