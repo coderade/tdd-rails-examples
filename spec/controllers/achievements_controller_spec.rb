cfacda4e9b9ad3c22be1d2c6978800321dd82434
Valdeci Gomes <coderade@outlook.com>
2017-06-07 22:55:06 -0300

    Add User Auth context on the  Achievement spec.

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -75,6 +75,116 @@ describe AchievementsController do
 
   end
 
+  describe 'authenticated_user' do
+    let(:user) {FactoryGirl.create :user}
+
+    before do
+      sign_in(user)
+    end
+
+    describe 'GET index' do
+      it 'renders :index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'assigns only public achievements to template' do
+        public_achievement = FactoryGirl.create(:public_achievement)
+        get :index
+        expect(assigns(:achievements)).to match_array([public_achievement])
+      end
+    end
+
+    describe 'GET show' do
+      let(:achievement) {FactoryGirl.create(:public_achievement)}
+
+      it 'renders :show template' do
+        get :show, params: {id: achievement}
+        expect(response).to render_template(:show)
+      end
+
+      it 'assigns requested achievement to @achievement' do
+        get :show, params: {id: achievement}
+        expect(assigns(:achievement)).to eq(achievement)
+      end
+    end
+
+    describe 'GET new' do
+      it 'renders :new template' do
+        get :new
+        expect(response).to render_template(:new)
+      end
+
+      it 'assigns new Achievement to @achievement' do
+        get :new
+        expect(assigns(:achievement)).to be_a_new(Achievement)
+      end
+
+    end
+
+    describe 'POST create' do
+      context 'valid data' do
+        let(:valid_data) {FactoryGirl.attributes_for(:public_achievement)}
+        it 'redirect to achievement#show' do
+          post :create, params: {achievement: valid_data}
+          expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+        end
+
+        it 'creates new achievement in database' do
+          expect {
+            post :create, params: {achievement: valid_data}
+          }.to change(Achievement, :count).by(1)
+        end
+      end
+
+      context 'invalid date' do
+        let(:invalid_data) {FactoryGirl.attributes_for(:public_achievement, title: '')}
+
+        it 'renders :new template' do
+          post :create, params: {achievement: invalid_data}
+          expect(response).to render_template(:new)
+        end
+
+        it "doesn't create new achievement in database" do
+          expect {
+            post :create, params: {achievement: invalid_data}
+          }.not_to change(Achievement, :count)
+        end
+      end
+    end
+
+    context 'is not the owner of the achievement' do
+      describe 'GET edit' do
+        it 'redirects to achievement page' do
+          get :edit, params: {id: FactoryGirl.create(:public_achievement)}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+
+      describe 'PUT update' do
+        let(:achievement) {FactoryGirl.create(:public_achievement)}
+        let(:valid_data) {FactoryGirl.attributes_for(:public_achievement, title: 'New Title')}
+
+        it 'redirects to achievement page' do
+          put :update, params: {id: achievement, achievement: valid_data}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+
+      describe 'DELETE destroy' do
+        let(:achievement) {FactoryGirl.create(:public_achievement)}
+
+        it 'redirects to achievement page' do
+          delete :destroy, params: {id: achievement}
+          expect(response).to redirect_to(achievements_path)
+        end
+      end
+    end
+
+    context 'is the owner of the achievement' do
+
+    end
+  end
 
   describe 'GET edit' do
     let(:achievement) {FactoryGirl.create(:public_achievement)}
@@ -89,7 +199,6 @@ describe AchievementsController do
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
-
 
   describe 'PUT update' do
     let(:achievement) {FactoryGirl.create(:public_achievement)}
@@ -138,7 +247,6 @@ describe AchievementsController do
     end
 
   end
-
 
   describe 'POST create' do
     context 'valid data' do
