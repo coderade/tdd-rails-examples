42c50cd4d653b7472f0ea8876cac4d792a485cf5
Valdeci Gomes <coderade@outlook.com>
2017-06-07 23:53:46 -0300

    Refactor Achievement controller spec.
    Refactor Achievement controller spec removing duplicate code.

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe AchievementsController do
 
-  describe 'guest_user' do
-
+  shared_examples 'public access to achievements' do
     describe 'GET index' do
       it 'renders :index template' do
         get :index
@@ -30,6 +29,11 @@ describe AchievementsController do
         expect(assigns(:achievement)).to eq(achievement)
       end
     end
+  end
+
+  describe 'guest_user' do
+
+    it_behaves_like 'public access to achievements'
 
     describe 'GET new' do
       it 'redirects to login page' do
@@ -82,32 +86,7 @@ describe AchievementsController do
       sign_in(user)
     end
 
-    describe 'GET index' do
-      it 'renders :index template' do
-        get :index
-        expect(response).to render_template(:index)
-      end
-
-      it 'assigns only public achievements to template' do
-        public_achievement = FactoryGirl.create(:public_achievement)
-        get :index
-        expect(assigns(:achievements)).to match_array([public_achievement])
-      end
-    end
-
-    describe 'GET show' do
-      let(:achievement) {FactoryGirl.create(:public_achievement)}
-
-      it 'renders :show template' do
-        get :show, params: {id: achievement}
-        expect(response).to render_template(:show)
-      end
-
-      it 'assigns requested achievement to @achievement' do
-        get :show, params: {id: achievement}
-        expect(assigns(:achievement)).to eq(achievement)
-      end
-    end
+    it_behaves_like 'public access to achievements'
 
     describe 'GET new' do
       it 'renders :new template' do
