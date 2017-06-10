b426a3cc68b33341b36bce68e57f22b4c57c1a13
Valdeci Gomes <coderade@outlook.com>
2017-06-10 20:11:04 -0300

    Add model testing with RSpec.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,10 +2,23 @@ class Achievement < ApplicationRecord
   belongs_to :user, optional: true
 
   validates_presence_of :title, { message: "Title can't be blank" }
+	validates_uniqueness_of :title, :scope => :user_id
+
+	# validate :unique_title_for_one_user
 
   enum privacy: [:public_access, :private_access, :friends_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description).html_safe
   end
+
+	private
+
+	# def unique_title_for_one_user
+  #   existing_achievement = Achievement.find_by(title: title)
+  #   if existing_achievement && existing_achievement.user == user
+  #     errors.add(:title, "you can't have two achievements with the same title.")
+	#
+  #   end
+  # end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'validations'do
+		it 'requires title' do
+			achievement = Achievement.new(title: '')
+
+			expect(achievement.valid?).to be_falsy
+		end
+
+		it 'requires title to be unique for one user' do
+			user = FactoryGirl.create(:user)
+			first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
+			new_achievement = Achievement.new(title: 'First Achievement', user: user)
+			expect(new_achievement.valid?).to be_falsy
+		end
+
+		it 'allows different user to have achievements with identical titles' do
+			user1 = FactoryGirl.create :user
+			user2 = FactoryGirl.create :user
+			first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user1)
+			new_achievement = Achievement.new(title: 'First Achievement', user: user2)
+
+			expect(new_achievement.valid?). to be_truthy
+
+
+		end
+	end
 end
+
