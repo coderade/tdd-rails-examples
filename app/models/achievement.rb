c776033094b6f53e194f9e0752ff2f853563c5aa
Valdeci Gomes <coderade@outlook.com>
2017-06-10 22:45:24 -0300

    Add Achievement model instance methods tests.

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -10,21 +10,13 @@ class Achievement < ApplicationRecord
 	}
 
 
-	# validate :unique_title_for_one_user
-
   enum privacy: [:public_access, :private_access, :friends_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description).html_safe
   end
 
-	private
-
-	# def unique_title_for_one_user
-  #   existing_achievement = Achievement.find_by(title: title)
-  #   if existing_achievement && existing_achievement.user == user
-  #     errors.add(:title, "you can't have two achievements with the same title.")
-	#
-  #   end
-  # end
+	def silly_title
+		"#{title} by #{user.email}"
+	end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe Achievement, type: :model do
 		it {should belong_to(:user)}
 	end
 
+	it 'converts markdown to html' do
+		achievement = Achievement.new(description: 'Awesome **thing** I *actually* did.')
+		expect(achievement.description_html).to include('<strong>thing</strong>')
+		expect(achievement.description_html).to include('<em>actually</em>')
+	end
+
+	let(:user) {FactoryGirl.create(:user, email: 'test@test.com')}
+	it 'has silly title' do
+		achievement = Achievement.new(title: 'New Achievement', user: user)
+		expect(achievement.silly_title).to eq('New Achievement by test@test.com')
+	end
 end
 
