f8e8a0d271fab3881322c403e8d0fbbdc4749756
Valdeci Gomes <coderade@outlook.com>
2017-07-06 20:37:30 -0300

    Fixes Route error on the encouragement spec.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :achievements
+  resources :achievements do
+		resources :encouragements, only: [:new, :create]
+	end
 
 	namespace :api do
 		resources :achievements, only: [:index] do
-			resources :encouragements, only: [:new, :create]
 		end
 	end
 
