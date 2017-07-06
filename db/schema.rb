3c52cc6043e5c498230cc4678439d658ca7e664a
Valdeci Gomes <coderade@outlook.com>
2017-07-06 20:55:14 -0300

    Update db schema file after Encouragement model add migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608015644) do
+ActiveRecord::Schema.define(version: 20170706235257) do
 
   create_table "achievements", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20170608015644) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_achievements_on_user_id"
+  end
+
+  create_table "encouragements", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "achievement_id"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["achievement_id"], name: "index_encouragements_on_achievement_id"
+    t.index ["user_id"], name: "index_encouragements_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
