d37e465d4227c3241b66128958d40cd9ef5a599f
Valdeci Gomes <coderade@outlook.com>
2017-07-06 20:53:52 -0300

    Update generate Encouragement model to add message field.

--- db/migrate/20170706235257_create_encouragements.rb
+++ db/migrate/20170706235257_create_encouragements.rb
@@ -3,6 +3,7 @@ class CreateEncouragements < ActiveRecord::Migration[5.1]
     create_table :encouragements do |t|
       t.references :user, foreign_key: true
       t.references :achievement, foreign_key: true
+      t.string :message
 
       t.timestamps
     end
--- spec/factories/encouragements.rb
+++ spec/factories/encouragements.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :encouragement do
     user nil
     achievement nil
+    message "MyString"
   end
 end
